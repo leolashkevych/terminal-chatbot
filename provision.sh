62673ab6cbf947b10ed2fc9545fe3dfc54f55649
Leo Lashkevych <leo@lashkevych.me>
2025-01-18 10:29:50 -0500

    initial harness

---
+++ provision.sh
@@ -0,0 +1,7 @@
+# https://www.llama.com/llama-downloads/
+# pip install llama-stack
+# llama model list
+# llama model download --source meta --model-id  Llama3.2-3B 
+
+brew install huggingface-cli   
+huggingface-cli login --token $HF_TOKEN 
